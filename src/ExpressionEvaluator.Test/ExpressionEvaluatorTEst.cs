2da397c425a868bfcb93ef8d45cb64d562027326
datchung <datxchung@gmail.com>
2020-05-06 21:20:25 -0400

    Add class library and test project

---
+++ src/ExpressionEvaluator.Test/ExpressionEvaluatorTEst.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ExpressionEvaluator.Test
+{
+    [TestFixture]
+    public class ExpressionEvaluatorTest
+    {
+        [Test]
+        public void Evaluate_NullExpression()
+        {
+            Assert.IsNull(ExpressionEvaluator.Evaluate(null));
+        }
+    }
+}
---
+++ src/ExpressionEvaluator/ExpressionEvaluator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpressionEvaluator
+{
+    public static class ExpressionEvaluator
+    {
+        public static object Evaluate(string expression)
+        {
+            if (string.IsNullOrWhiteSpace(expression)) return null;
+
+            return null;
+        }
+    }
+}
