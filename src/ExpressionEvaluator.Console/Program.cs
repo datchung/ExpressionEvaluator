cebe0370697a40c354467704ae343594273b2468
datchung <datxchung@gmail.com>
2020-05-11 21:33:02 -0400

    Add console app to test nuget package install and usage

---
+++ src/ExpressionEvaluator.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpressionEvaluator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(ExpressionEvaluator.Evaluate("7 / 2"));
+            System.Console.ReadKey();
+        }
+    }
+}
